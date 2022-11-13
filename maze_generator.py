1547d55a29c14fb75c6e23feb5a137bd7090d6d2
eg0rmeister <karpuhin.egor@gmail.com>
2022-11-13 13:49:44 +0300

    implemented DFSMaze generator

--- maze_generator.py
+++ maze_generator.py
@@ -66,24 +66,91 @@ def getWall(walls):
         return NESW_s
 
 class Maze:
-    __width = 0
+    _width = 0
     def get_width(self):
-        return self.__width
+        return self._width
     width = property(get_width)
     
-    __height = 0
+    _height = 0
     def get_height(self):
-        return self.__height
-    height = property(lambda self: self.__height)
+        return self._height
+    height = property(get_height)
     
     def __init__(self, width, height):
         if (width < 2 or height < 2):
             raise Exception
-        self.__height = height
-        self.__width = width
+        self._height = height
+        self._width = width
+        self._cells = [[2 for i in range(2 * self.width + 1)]]
+        for i in range(2 * self.height - 1):
+            self._cells.append([2])
+            for j in range(2 * self.width - 1):
+                self._cells[-1].append(1 if i & 1 or j & 1 else 0)
+            self._cells[-1].append(2)
+        self._cells.append([2 for i in range(2 * self.width + 1)])
     
     def GenerateMaze(self):
         raise NotImplementedError
     
     def ShowMaze(self):
-        raise NotImplementedError
+        for i in range(2 * self.height + 1):
+            for j in range(2 * self.width + 1):
+                if self._cells[i][j] == 0:
+                    print(' ', end='')
+                else:
+                    top = 0
+                    right = 0
+                    bottom = 0
+                    left = 0
+                    if i != 0:
+                        top = self._cells[i - 1][j] != 0
+                    if j != 2 * self.width:
+                        right = self._cells[i][j + 1] != 0
+                    if i != 2 * self.height:
+                        bottom = self._cells[i + 1][j] != 0
+                    if j != 0:
+                        left = self._cells[i][j - 1] != 0
+                    print(getWall((top << 3) +
+                                  (right << 2) +
+                                  (bottom << 1) +
+                                  (left << 0)), end='')
+            print()
+                    
+class DFSMaze(Maze):
+    def GenerateMaze(self):
+        visited = set((1, 1))
+        stack = [(1, 1)]
+        stack_len = 1
+        while stack_len != 0:
+            can_visit = []
+            to_north = (stack[-1][0] - 2, stack[-1][1])
+            to_east = (stack[-1][0], stack[-1][1] + 2)
+            to_south = (stack[-1][0] + 2, stack[-1][1])
+            to_west = (stack[-1][0], stack[-1][1] - 2)
+            
+            if (to_north[0] > 0 and to_north not in visited):
+                can_visit.append(to_north)
+            if (to_east[1] < 2 * self.width and to_east not in visited):
+                can_visit.append(to_east)
+            if (to_south[0] < 2 * self.height and to_south not in visited):
+                can_visit.append(to_south)
+            if (to_west[1] > 0 and to_west not in visited):
+                can_visit.append(to_west)
+            if len(can_visit) == 0:
+                stack.pop()
+                stack_len -= 1
+            else:
+                to_visit = random.choice(can_visit)
+                if (to_visit == to_north):
+                    self._cells[stack[-1][0] - 1][stack[-1][1]] = 0
+                if (to_visit == to_east):
+                    self._cells[stack[-1][0]][stack[-1][1] + 1] = 0
+                if (to_visit == to_south):
+                    self._cells[stack[-1][0] + 1][stack[-1][1]] = 0
+                if (to_visit == to_west):
+                    self._cells[stack[-1][0]][stack[-1][1] - 1] = 0
+                stack_len += 1
+                stack.append(to_visit)
+                visited.add(stack[-1])
+        self._cells[0][1] = 0
+        self._cells[2 * self.height][2 * self.width - 1] = 0
