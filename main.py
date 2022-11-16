431765524dc753486a1b083fb0a20ce8179e92b2
eg0rmeister <karpuhin.egor@gmail.com>
2022-11-16 21:08:18 +0300

    added the ability to exit without printing solution

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ if (sys.argv[1].lower() == "spanning_tree"):
 elif (sys.argv[1].lower() == "dfs"):
     maze = maze_generator.DFSMaze(int(sys.argv[2]), int(sys.argv[3]))
 if (sys.argv[4] == "play"):
-    won = True
     while not maze.status:
         print('\n')
         maze.ShowGame()
         move = input("enter your move(wasd) or 'help' for available commands':")
         if (move == "help"):
             print("\thelp -- display these tips")
-            print("\texit -- exit the game and display the solution")
+            print("\texit and solve -- exit the game and display the solution")
+            print("\texit -- exit the game WITHOUT displaying solution")
             print("\tsolution -- display the solution and continue playing")
             print("\tsave <filename> -- save the game to file named <filename>")
             print("\tload <filename> -- load the game from file named <filename>")
@@ -23,12 +23,14 @@ if (sys.argv[4] == "play"):
         elif (move[:5] == "load "):
             maze.Load(move[5:])
         elif (move == "exit"):
-            won = False
+            break
+        elif (move == "exit and solve"):
+            maze.ShowSolution()
             break
         elif not maze.Move(move):
             print("that move is prohibited")
-    maze.ShowSolution()
-    if won:
+    if maze.status:
+        maze.ShowSolution()
         print("You Won!")
 elif (sys.argv[4].lower() == "show"):
     maze.ShowSolution()
