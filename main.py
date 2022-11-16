af89e449d609e3c7701ad917de3df65fa004ab0c
eg0rmeister <karpuhin.egor@gmail.com>
2022-11-16 20:55:52 +0300

    added the ability for user to save, load, exit, display solution, call for help

--- main.py
+++ main.py
@@ -9,12 +9,28 @@ if (sys.argv[4] == "play"):
     while not maze.status:
         print('\n')
         maze.ShowGame()
-        move = input("enter your move(wasd) or 'exit' to exit:")
-        if (move == "exit"):
+        move = input("enter your move(wasd) or 'help' for available commands':")
+        if (move == "help"):
+            print("\thelp -- display these tips")
+            print("\texit -- exit the game and display the solution")
+            print("\tsolution -- display the solution and continue playing")
+            print("\tsave <filename> -- save the game to file named <filename>")
+            print("\tload <filename> -- load the game from file named <filename>")
+        elif (move == "solution"):
+            maze.ShowSolution()
+        elif (move[:5] == "save "):
+            maze.Save(move[5:])
+        elif (move[:5] == "load "):
+            maze.Load(move[5:])
+        elif (move == "exit"):
             won = False
             break
-        if not maze.Move(move):
+        elif not maze.Move(move):
             print("that move is prohibited")
     maze.ShowSolution()
     if won:
         print("You Won!")
+elif (sys.argv[4].lower() == "show"):
+    maze.ShowSolution()
+    print('\n\n')
+    maze.Show()
